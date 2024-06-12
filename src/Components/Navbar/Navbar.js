0d3be659a444d37e77aef4f596020960a9093561
RaymondBog <raymondbog@gmail.com>
2024-06-12 13:28:27 +0000

    Navbar

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Navbar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faShoppingCart,faCircleUser } from '@fortawesome/free-solid-svg-icons';
+import login from '../Assets/login.jpg'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <ul className="nav-menu">
+            <li><button>Home</button></li>
+            <li><button>Shop</button></li>
+            <li><button>Mtn Bundle</button></li>
+            <li><button>Tigo Bundle</button></li>
+        </ul>
+        <div className="login-cart">
+            <FontAwesomeIcon icon={faShoppingCart} style={{width:'20px',height:'30px'}}/>
+            <div className="cart-count">0</div>
+            <button style={{background:'rgba(51, 51, 51, 1)'}}>
+            <FontAwesomeIcon icon={faCircleUser} style={{color:'white'}} />
+            </button>
+        </div>
+
+      
+    </div>
+  )
+}
+
+export default Navbar
