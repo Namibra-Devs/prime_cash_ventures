7c5c8f82ad21851fef542ad988d97f93d0e78bde
RaymondBog <raymondbog@gmail.com>
2024-06-18 15:14:08 +0000

    working on landing page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -103,21 +103,157 @@ const Home = () => {
         </div>
 
         <div className="right-side">
+            <div className="at" style={{background:'rgba(255, 255, 255, 1)',padding:'10px',marginTop:'40px'}}>
             <div className="input">
                 <input type="text" placeholder='Search for products and offers'style={{width:'300px'}} /> <button style={{width:'100px'}}>Search</button>
             </div>
-            <div className='at-rates' style={{display:'flex'}}>
-                <div className="rates-img">
-                <img src={at} alt="" />
+            <div className='at-rates' style={{display:'flex', gap:'10px'}}>
+            
+                <div>
+                   <img style={{width:'200px'}} src={at} alt="" />
+                </div>
+
+                <div>
                 <div className="rate-text">
                     <h3>AIRTEL TIGO UNLIMITED INTERNET <br />BUNDLES</h3>
                     <p>Very affordable at all prices and last longer ranges from</p>
+
+                  <div className="rate-text-prices" style={{display:'flex'}}>
+                   <div className="rate-text-prices1" style={{marginRight:'10px'}}>
+                        <ul>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                        </ul>
+                   </div>
+
+                   <div className="rate-text-prices2">
+                        <ul>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                        </ul>
+                   </div>
+                  </div>  
+
                 </div>
-            </div>
+                </div>
                 
 
 
             </div>
+            </div>
+
+            <div className="mtn" style={{background:'rgba(255, 255, 255, 1)',padding:'10px',marginTop:'40px'}}>
+            <div className='mtn-rates' style={{display:'flex', gap:'10px'}}>
+            
+                <div>
+                   <img style={{width:'200px'}} src={mtn} alt="" />
+                </div>
+
+                <div>
+                <div className="rate-text">
+                    <h3>AIRTEL TIGO UNLIMITED INTERNET <br />BUNDLES</h3>
+                    <p>Very affordable at all prices and last longer ranges from</p>
+
+                  <div className="rate-text-prices" style={{display:'flex'}}>
+                   <div className="rate-text-prices1" style={{marginRight:'10px'}}>
+                        <ul>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                        </ul>
+                   </div>
+
+                   <div className="rate-text-prices2">
+                        <ul>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                            <li>1GB = Ghs 4.00</li>
+                        </ul>
+                   </div>
+                  </div>  
+
+                </div>
+                </div>
+                
+
+
+            </div>
+            </div>
+            <div className="accessories" style={{background:'rgba(255, 255, 255, 1)',padding:'10px',marginTop:'40px'}}>
+                <p>Our online shop we deals in all electronic accessories</p>
+                <div className="accessory-items1">
+                    <div className='accessory-img'>
+                       <img src={pad} alt="" />
+                    </div>
+                    <div className="accessory-text">
+                      <h3>This MacBook Pro 16-inch</h3>
+                      <h4>Color: Gray</h4>
+                      <p>Apple's latest laptop with a stunning display and sleek design. <br/>Featuring the powerful M1 or M1 Max chip, this device is perfect <br />for both iOS and macOS users.</p>
+                    </div>
+                </div>
+
+                <div className="accessory-items2">
+                    <div className='accessory-img'>
+                       <img src={laptop} alt="" />
+                    </div>
+                    <div className="accessory-text">
+                      <h3>This MacBook Pro 16-inch</h3>
+                      <h4>Color: Gray</h4>
+                      <p>Apple's latest laptop with a stunning display and sleek design. <br/>Featuring the powerful M1 or M1 Max chip, this device is perfect <br />for both iOS and macOS users.</p>
+                    </div>
+                </div>
+
+                <div className="accessory-items3">
+                    <div className='accessory-img'>
+                       <img src={rerouter} alt="" />
+                    </div>
+                    <div className="accessory-text">
+                      <h3>This MacBook Pro 16-inch</h3>
+                      <h4>Color: Gray</h4>
+                      <p>Apple's latest laptop with a stunning display and sleek design. <br/>Featuring the powerful M1 or M1 Max chip, this device is perfect <br />for both iOS and macOS users.</p>
+                    </div>
+                </div>
+
+                <div className="accessory-items4">
+                    <div className='accessory-img'>
+                       <img src={rerouter} alt="" />
+                    </div>
+                    <div className="accessory-text">
+                      <h3>This MacBook Pro 16-inch</h3>
+                      <h4>Color: Gray</h4>
+                      <p>Apple's latest laptop with a stunning display and sleek design. <br/>Featuring the powerful M1 or M1 Max chip, this device is perfect <br />for both iOS and macOS users.</p>
+                    </div>
+                </div>
+
+                <button>Shop now</button>
+            </div>
+           
 
         </div>
         
